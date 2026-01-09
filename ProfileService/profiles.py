a0c037de84404093494911b0cff5267c325b94ba
Noah-cmp <noah.compton@students.plymouth.ac.uk>
2026-01-09 10:46:54 +0000

    profiles CRUD operations

--- ProfileService/profiles.py
+++ ProfileService/profiles.py
@@ -28,3 +28,61 @@ def _validate(profile: dict, is_create: bool):
         abort(400, "password must be <= 30 characters")
 
 
+def read_all():
+    profiles = Profile.query.all()
+    return profiles_schema.dump(profiles), 200
+
+
+def read_one(email: str):
+    profile = Profile.query.get(email)
+    if profile is None:
+        abort(404, f"Profile with email {email} not found")
+    return profile_schema.dump(profile), 200
+
+
+def create(profile: dict):
+    _validate(profile, is_create=True)
+
+    existing = Profile.query.get(profile["email"])
+    if existing is not None:
+        abort(409, "Profile with this email already exists")
+
+    if "role" not in profile:
+        profile["role"] = "User"
+
+    new_profile = profile_schema.load(profile, session=db.session)
+    db.session.add(new_profile)
+    db.session.commit()
+    return profile_schema.dump(new_profile), 201
+
+
+def update(email: str, profile: dict):
+    _validate(profile, is_create=False)
+
+    existing = Profile.query.get(email)
+    if existing is None:
+        abort(404, f"Profile with email {email} not found")
+
+    for field in ["username", "aboutme", "location", "dob", "language", "password", "role"]:
+        if field in profile:
+            setattr(existing, field, profile[field])
+
+    db.session.merge(existing)
+    db.session.commit()
+    return profile_schema.dump(existing), 200
+
+
+def delete(email: str):
+    existing = Profile.query.get(email)
+    if existing is None:
+        abort(404, f"Profile with email {email} not found")
+
+    db.session.delete(existing)
+    db.session.commit()
+    return make_response(f"{email} deleted", 200)
+
+
+
+
+
+
