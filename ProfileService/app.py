82741d2091d78c8d93bd00173124fda8cfeaac12
Noah-cmp <noah.compton@students.plymouth.ac.uk>
2026-01-09 11:44:43 +0000

    Adding html to view and troubleshooting

--- ProfileService/app.py
+++ ProfileService/app.py
@@ -3,9 +3,12 @@ import config
 from models import Profile
 
 app = config.connex_app
-app.add_api(config.basedir / "swagger.yml")
+app.add_api(config.basedir / "swagger.yaml")
 
 @app.route("/")
 def home():
     people = Profile.query.all()
     return render_template("home.html", Profile=Profile)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
